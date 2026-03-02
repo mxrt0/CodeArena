b547b6441e5c48a5db2913e0a43828a58ab19f07
Martin Terziev <m.terziev08@abv.bg>
2026-03-02 17:39:44 +0200

    Implemented submission repository.

---
+++ CodeArena.Data/Repositories/Contracts/ISubmissionRepository.cs
@@ -0,0 +1,13 @@
+﻿using CodeArena.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories.Contracts;
+
+public interface ISubmissionRepository
+{
+    Task AddAsync(Submission submission);
+}
---
+++ CodeArena.Data/Repositories/SubmissionRepository.cs
@@ -0,0 +1,25 @@
+﻿using CodeArena.Data.Models;
+using CodeArena.Data.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories;
+
+public class SubmissionRepository : ISubmissionRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public SubmissionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Submission submission)
+    {
+        _context.Submissions.Add(submission);
+        await _context.SaveChangesAsync();
+    }
+}
