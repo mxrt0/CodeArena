c527ef56c6958282947453d7918211264753fc46
Martin Terziev <m.terziev08@abv.bg>
2026-03-02 17:42:48 +0200

    Implemented submission service with adding functionality.

---
+++ CodeArena.Services/Core/Contracts/ISubmissionService.cs
@@ -0,0 +1,14 @@
+﻿using CodeArena.Services.DTOs.Submission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.Core.Contracts;
+
+public interface ISubmissionService
+{
+    Task CreateSubmissionAsync(SubmissionCreateDto createDto, ClaimsPrincipal user);
+}
---
+++ CodeArena.Services/Core/SubmissionService.cs
@@ -0,0 +1,46 @@
+﻿using CodeArena.Data.Common.Enums;
+using CodeArena.Data.Models;
+using CodeArena.Data.Repositories.Contracts;
+using CodeArena.Services.Core.Contracts;
+using CodeArena.Services.DTOs.Submission;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.Core;
+
+public class SubmissionService : ISubmissionService
+{
+    private readonly ISubmissionRepository _repository;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public SubmissionService(
+        ISubmissionRepository repository,
+        UserManager<ApplicationUser> userManager
+        )
+    {
+        _repository = repository;
+        _userManager = userManager;
+    }
+    public async Task CreateSubmissionAsync(SubmissionCreateDto createDto, ClaimsPrincipal user)
+    {
+        var userId = _userManager.GetUserId(user);
+        if (userId is null)
+        {
+            throw new InvalidOperationException("User must be authenticated to create a submission.");
+        }
+        var submission = new Submission
+        {
+            ChallengeId = createDto.ChallengeId,
+            SolutionCode = createDto.SolutionCode,
+            Language = createDto.Language,
+            UserId = userId,
+            Status = SubmissionStatus.Pending
+        };
+        await _repository.AddAsync(submission);
+    }
+}
