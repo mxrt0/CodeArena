0a0488738e61281428bc187a96240a4b2cf9612c
Martin Terziev <m.terziev08@abv.bg>
2026-03-02 16:05:07 +0200

    Implemented viewing challenge details.

---
+++ CodeArena.Web/Models/Challenge/ChallengeDetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CodeArena.Services.DTOs.Challenge;
+
+namespace CodeArena.Web.Models.Challenge;
+
+public class ChallengeDetailsViewModel
+{
+    public ChallengeDisplayDto Challenge { get; set; } = null!;
+}
