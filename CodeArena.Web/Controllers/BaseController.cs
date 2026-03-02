2abd01179755c86179053fe092a3dbacf3e7e4d1
Martin Terziev <m.terziev08@abv.bg>
2026-03-02 16:03:06 +0200

    Added base controller for future custom controllers to inherit.

---
+++ CodeArena.Web/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CodeArena.Web.Controllers;
+
+[Authorize]
+public class BaseController : Controller
+{
+    public string? GetUserId()
+    {
+        return User.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
