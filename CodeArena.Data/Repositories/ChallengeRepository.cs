d362162d2b5541014b3045050c6ed6cb3db3f9fd
Martin Terziev <m.terziev08@abv.bg>
2026-03-02 15:50:11 +0200

    Implemented challenge repository

---
+++ CodeArena.Data/Repositories/ChallengeRepository.cs
@@ -0,0 +1,53 @@
+﻿using CodeArena.Data.Models;
+using CodeArena.Data.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories;
+
+public class ChallengeRepository : IChallengeRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public ChallengeRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task AddAsync(Challenge challenge)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Challenge?> GetByIdAsync(int id)
+    {
+        return await _context.Challenges
+            .AsNoTracking()
+            .Include(c => c.Submissions)
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<IEnumerable<Challenge>> GetChallengesAsync()
+    {
+        var query = _context.Challenges
+            .AsNoTracking()
+            .Include(c => c.Submissions)
+            .OrderBy(c => c.Difficulty);
+
+        return await query.ToListAsync();
+    }
+
+    public Task UpdateAsync(Challenge challenge)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CodeArena.Data/Repositories/Contracts/IChallengeRepository.cs
@@ -0,0 +1,17 @@
+﻿using CodeArena.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories.Contracts;
+
+public interface IChallengeRepository
+{
+    Task<IEnumerable<Challenge>> GetChallengesAsync();
+    Task<Challenge?> GetByIdAsync(int id);
+    Task AddAsync(Challenge challenge);
+    Task UpdateAsync(Challenge challenge);
+    Task DeleteAsync(int id);
+}
