717c83f2624a5accf992ee77984c938f1106e153
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 10:33:04 +0200

    Created DTO for submission review.

---
+++ CodeArena.Services/DTOs/Admin/Submission/AdminSubmissionReviewDto.cs
@@ -0,0 +1,19 @@
+﻿using CodeArena.Data.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.DTOs.Admin.Submission;
+
+public sealed record AdminSubmissionReviewDto(
+    int SubmissionId,
+    string ChallengeTitle,
+    string UserDisplayName,
+    SubmissionLanguage Language,
+    Difficulty Difficulty,
+    string SolutionCode,
+    DateTime SubmittedAt
+);
+
