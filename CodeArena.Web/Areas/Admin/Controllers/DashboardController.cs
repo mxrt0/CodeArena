b7243e2b57b0178590eefe8707f88e6b67e19be1
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 17:25:57 +0200

    Added dashboard and submission controllers (admin)

---
+++ CodeArena.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeArena.Web.Areas.Admin.Controllers;
+
+public class DashboardController : BaseAdminController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ CodeArena.Web/Areas/Admin/Controllers/SubmissionsController.cs
@@ -0,0 +1,19 @@
+﻿using CodeArena.Services.Core.Admin.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeArena.Web.Areas.Admin.Controllers;
+
+public class SubmissionsController : BaseAdminController
+{
+    private readonly IAdminSubmissionService _submissionService;
+    public SubmissionsController(IAdminSubmissionService submissionService)
+    {
+        _submissionService = submissionService;
+    }
+
+    public async Task<IActionResult> Pending()
+    {
+        var submissions = await _submissionService.GetPendingSubmissionsAsync();
+        return View(submissions);
+    }
+}
