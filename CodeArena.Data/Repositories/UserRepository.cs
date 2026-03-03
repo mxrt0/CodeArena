e37165d0d336e3fc93ce98ae21ad7ee7f262295b
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 17:35:19 +0200

    Implemented user repository with basic methods.

---
+++ CodeArena.Data/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using CodeArena.Data.Models;
+using CodeArena.Data.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(ApplicationUser user)
+    {
+        await _context.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<ApplicationUser, bool>> predicate)
+    {
+        return await _context.Users.AnyAsync(predicate);
+    }
+
+    public Task<int> CountAsync(Expression<Func<ApplicationUser, bool>>? predicate = null)
+    {
+        return predicate is null
+        ? _context.Users.CountAsync()
+        : _context.Users.CountAsync(predicate);
+    }
+
+    public async Task<ApplicationUser?> FirstOrDefaultAsync(Expression<Func<ApplicationUser, bool>> predicate)
+    {
+        return await _context.Users.FirstOrDefaultAsync(predicate);
+    }
+
+    public IQueryable<ApplicationUser> GetAll() => _context.Users.AsNoTracking();
+
+    public async Task RemoveAsync(ApplicationUser user)
+    {
+        _context.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+}
