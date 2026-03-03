f1ccc3801892d43c105ba595c40de3cc69228fae
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 17:25:10 +0200

    Added admin dashboard VM,

---
+++ CodeArena.Web/Areas/Admin/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CodeArena.Services.DTOs.Admin.Dashboard;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeArena.Web.Areas.Admin.Models;
+
+public class AdminDashboardViewModel
+{
+    AdminDashboardDto DashboardData { get; set; } = null!;
+} 
