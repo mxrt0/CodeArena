b30da57ca22d5521f88662b39f7b47f6f6154cee
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 10:50:51 +0200

    Created admin-specific submission service and implemented method for getting pending submissions.

---
+++ CodeArena.Services/Core/Admin/AdminSubmissionService.cs
@@ -0,0 +1,42 @@
+﻿using CodeArena.Data.Repositories.Contracts;
+using CodeArena.Services.Core.Admin.Contracts;
+using CodeArena.Services.DTOs.Admin.Submission;
+using CodeArena.Data.Common.Enums;
+using static CodeArena.Common.ApplicationConstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeArena.Services.Core.Admin;
+
+public class AdminSubmissionService : IAdminSubmissionService
+{
+    private readonly ISubmissionRepository _repository;
+
+    public AdminSubmissionService(ISubmissionRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<AdminSubmissionReviewDto>> GetPendingSubmissionsAsync()
+    {
+        var submissions = _repository.GetAll()
+            .Where(s => s.Status == SubmissionStatus.Pending)
+            .Include(s => s.Challenge)
+            .Include(s => s.User);
+
+        return await submissions.Select(s => new AdminSubmissionReviewDto
+        (
+            s.Id,
+            s.Challenge.Title,
+            s.User.DisplayName,
+            s.Language.ToString(),
+            s.Challenge.Difficulty.ToString(),
+            s.SolutionCode,
+            s.SubmittedAt.ToString(DefaultDateFormat)
+        )).ToListAsync();
+    }
+}
---
+++ CodeArena.Services/Core/Admin/Contracts/IAdminSubmissionService.cs
@@ -0,0 +1,13 @@
+﻿using CodeArena.Services.DTOs.Admin.Submission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.Core.Admin.Contracts;
+
+public interface IAdminSubmissionService
+{
+    Task<IEnumerable<AdminSubmissionReviewDto>> GetPendingSubmissionsAsync();
+}
