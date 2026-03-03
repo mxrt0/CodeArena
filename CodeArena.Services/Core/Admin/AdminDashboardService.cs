9d1e40cdf8791f84d15d290a15393ab17ad5f7b8
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 17:26:21 +0200

    Added admin dashboard service.

---
+++ CodeArena.Services/Core/Admin/AdminDashboardService.cs
@@ -0,0 +1,17 @@
+﻿using CodeArena.Services.Core.Admin.Contracts;
+using CodeArena.Services.DTOs.Admin.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.Core.Admin;
+
+public class AdminDashboardService : IAdminDashboardService
+{
+    public Task<AdminDashboardDto> GetDashboardDataAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
