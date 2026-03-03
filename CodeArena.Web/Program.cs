039f6a4fe4e3317a9d61c1f292c34cf047d7dad8
Martin Terziev <m.terziev08@abv.bg>
2026-03-03 17:58:20 +0200

    Made area route mapper default to Dashboard controller (suited for Admin)

--- CodeArena.Web/Program.cs
+++ CodeArena.Web/Program.cs
@@ -80,7 +80,7 @@ namespace CodeArena.Web
 
             app.MapControllerRoute(
                 name: "areas",
-                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
                 name: "default",
