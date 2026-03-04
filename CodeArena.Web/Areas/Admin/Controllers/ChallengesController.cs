5f540b3ce800e354af01765fb5bf50a84cac32e7
Martin Terziev <m.terziev08@abv.bg>
2026-03-04 16:04:51 +0200

    Created submissions controller and implemented create actions.

---
+++ CodeArena.Web/Areas/Admin/Controllers/ChallengesController.cs
@@ -0,0 +1,42 @@
+﻿using CodeArena.Services.Core.Admin.Contracts;
+using CodeArena.Services.DTOs.Admin.Challenge;
+using CodeArena.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeArena.Web.Areas.Admin.Controllers;
+
+public class ChallengesController : BaseAdminController
+{
+    private readonly IAdminChallengeService _challengeService;
+
+    public ChallengesController(IAdminChallengeService challengeService)
+    {
+        _challengeService = challengeService;
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CreateChallengeDto dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            var vm = new CreateChallengeViewModel
+            {
+                Challenge = dto
+            };
+            return View(vm);
+        }
+        await _challengeService.CreateChallengeAsync(dto);
+        return RedirectToAction(nameof(Index));
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
