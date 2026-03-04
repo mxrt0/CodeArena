e9ed47dab9efc9ddaf977976a81d63bb6f18613e
Martin Terziev <m.terziev08@abv.bg>
2026-03-04 16:45:09 +0200

    Added optional tags and validation for create challenge dto properties.

--- CodeArena.Services/DTOs/Admin/Challenge/CreateChallengeDto.cs
+++ CodeArena.Services/DTOs/Admin/Challenge/CreateChallengeDto.cs
@@ -12,13 +12,16 @@ namespace CodeArena.Services.DTOs.Admin.Challenge;
 public class CreateChallengeDto
 {
     [Required]
-    [StringLength(TitleMaxLength)]
+    [StringLength(TitleMaxLength, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters long. ")]
     public string Title { get; set; } = null!;
 
     [Required]
-    [StringLength(DescriptionMaxLength)]
+    [StringLength(DescriptionMaxLength, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 2000 characters long.")]
     public string Description { get; set; } = null!;
 
     [Required]
     public Difficulty Difficulty { get; set; }
+
+    [Display(Name = "Tags (optional)")]
+    public string? Tags { get; set; }
 }
