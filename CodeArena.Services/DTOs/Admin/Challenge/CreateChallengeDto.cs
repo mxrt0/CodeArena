d2d152581bd4821b5847a076e7a22a514cf9cdbb
Martin Terziev <m.terziev08@abv.bg>
2026-03-04 15:58:19 +0200

    Added DTO for creating challenge.

---
+++ CodeArena.Services/DTOs/Admin/Challenge/CreateChallengeDto.cs
@@ -0,0 +1,24 @@
+﻿using CodeArena.Data.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CodeArena.Data.Common.EntityValidation.Challenge;
+
+namespace CodeArena.Services.DTOs.Admin.Challenge;
+
+public class CreateChallengeDto
+{
+    [Required]
+    [StringLength(TitleMaxLength)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [StringLength(DescriptionMaxLength)]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    public Difficulty Difficulty { get; set; }
+}
