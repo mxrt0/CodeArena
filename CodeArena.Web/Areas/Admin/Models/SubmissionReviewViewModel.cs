c7657bd14720db2117e57533d6bffe67662d8ecd
Martin Terziev <m.terziev08@abv.bg>
2026-03-06 19:25:22 +0200

    Added submission review VM.

---
+++ CodeArena.Web/Areas/Admin/Models/SubmissionReviewViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CodeArena.Services.DTOs.Admin.Submission;
+
+namespace CodeArena.Web.Areas.Admin.Models;
+
+public class SubmissionReviewViewModel
+{
+    public AdminSubmissionReviewDto Submission { get; set; } = null!;
+}
