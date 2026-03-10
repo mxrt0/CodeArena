fe7482ee92ec89f13fd1ae7e087d49e396e5fb0d
Martin Terziev <m.terziev08@abv.bg>
2026-03-10 16:57:23 +0200

    Added dto for showing details for submissions.

---
+++ CodeArena.Services/DTOs/Submission/SubmissionDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.DTOs.Submission;
+
+public sealed record SubmissionDetailsDto(
+    int SubmissionId,
+    string ChallengeTitle,
+    string Language,
+    string Status,
+    string? Feedback,
+    string SolutionCode,
+    DateTime SubmittedAt
+);
+
+
