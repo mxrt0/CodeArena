b7774dbc17f8e6813f0fe8652ab6fd49f2a4b704
Martin Terziev <m.terziev08@abv.bg>
2026-03-13 17:31:43 +0200

    Created profile controller and implemented Stats action.

---
+++ CodeArena.Web/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using CodeArena.Services.Core.Contracts;
+using CodeArena.Web.Models.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+
+namespace CodeArena.Web.Controllers;
+
+public class ProfileController : BaseController
+{
+    private readonly IUserService _userService;
+
+    public ProfileController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public async Task<IActionResult> Stats()
+    {
+        var stats = await _userService.GetUserStatsAsync(User);
+
+        var vm = new UserStatsViewModel
+        {
+            Stats = stats
+        };
+
+        return View(vm);
+    }
+}
