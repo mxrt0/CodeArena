12d1999a635a2f245ed107f6cbe95875bb9dd19d
Martin Terziev <m.terziev08@abv.bg>
2026-03-19 21:45:06 +0200

    Adjusted global script to raise custom theme change event which the codeMirror script listens for.

--- CodeArena.Web/wwwroot/js/site.js
+++ CodeArena.Web/wwwroot/js/site.js
@@ -5,20 +5,27 @@
 const toggleBtn = document.getElementById("themeToggle");
 const body = document.getElementById("appBody");
 const icon = toggleBtn?.querySelector("i");
+const hljsDarkTheme = document.getElementById('hljs-theme-dark');
 
 if (localStorage.getItem("theme") === "dark") {
     body.classList.add("dark-mode");
-    document.getElementById('hljs-theme-dark').disabled = false;
+    if (hljsDarkTheme) {
+        hljsDarkTheme.disabled = false;
+    }
     if (icon) icon.classList.replace("bi-moon", "bi-sun");
 }
 
 toggleBtn?.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
-
     const isDark = body.classList.contains("dark-mode");
-    document.getElementById('hljs-theme-dark').disabled = !isDark;
-    localStorage.setItem("theme", isDark ? "dark" : "light");
 
+    if (hljsDarkTheme) {
+        hljsDarkTheme.disabled = !isDark;
+    }
+
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    const themeEvent = new CustomEvent("themeChanged", { detail: { isDark: isDark } });
+    document.dispatchEvent(themeEvent);
     if (icon) {
         icon.classList.toggle("bi-moon");
         icon.classList.toggle("bi-sun");
