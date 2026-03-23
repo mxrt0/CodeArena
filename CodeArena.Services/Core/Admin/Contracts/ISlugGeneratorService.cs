c1209776b00fd7b010cf664fbd6c1040ab3ab8f7
Martin Terziev <m.terziev08@abv.bg>
2026-03-23 18:37:46 +0200

    Implemented slug generator service.

---
+++ CodeArena.Services/Core/Admin/Contracts/ISlugGeneratorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.Core.Admin.Contracts;
+
+public interface ISlugGeneratorService
+{
+    Task<string> GenerateUniqueSlugAsync(string input);
+}
