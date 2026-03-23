2713870c03c4bcab54c5ad29fba27554f49bf966
Martin Terziev <m.terziev08@abv.bg>
2026-03-23 19:21:07 +0200

    Extracted slug generator into common utility class and removed redundant service implementation.

---
+++ CodeArena.Common/Utilities/SlugGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeArena.Common.Utilities;
+
+public static class SlugGenerator
+{
+    public static string Generate(string input)
+    {
+        var slug = input.ToLowerInvariant();
+
+        slug = Regex.Replace(slug, @"[^a-z0-9\s-]", "");
+        slug = Regex.Replace(slug, @"\s+", " ").Trim();
+        slug = slug.Replace(" ", "-");
+
+        return slug;
+    }
+}
