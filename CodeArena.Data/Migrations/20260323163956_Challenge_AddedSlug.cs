e09b0d36822c1be8cbd6697b9e6573eab9f65146
Martin Terziev <m.terziev08@abv.bg>
2026-03-23 19:19:42 +0200

    Ran migration for adding slug (nullable for now)

---
+++ CodeArena.Data/Migrations/20260323163956_Challenge_AddedSlug.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeArena.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Challenge_AddedSlug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Challenges",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "Slug" },
+                values: new object[] { new DateTime(2026, 3, 23, 16, 39, 54, 675, DateTimeKind.Utc).AddTicks(449), null });
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "Slug" },
+                values: new object[] { new DateTime(2026, 3, 23, 16, 39, 54, 675, DateTimeKind.Utc).AddTicks(456), null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Challenges");
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 5, 18, 8, 30, 863, DateTimeKind.Utc).AddTicks(5181));
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 5, 18, 8, 30, 863, DateTimeKind.Utc).AddTicks(5188));
+        }
+    }
+}
