9fba1e2975eef1ed8b1ea0b035b2006db475b560
Martin Terziev <m.terziev08@abv.bg>
2026-03-25 19:41:58 +0200

    Added new projects for services, web and integration tests.

---
+++ CodeArena.Services.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodeArena.Services.Tests;
+
+[TestFixture]
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
