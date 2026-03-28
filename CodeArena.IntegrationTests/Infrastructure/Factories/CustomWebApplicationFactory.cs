ffc229e37114c85352e46be76d10952104f65d78
Martin Terziev <m.terziev08@abv.bg>
2026-03-28 19:58:52 +0200

    Configured scope of test db guid name to be within the factory lifetime to avoid scope issues.

--- CodeArena.IntegrationTests/Infrastructure/Factories/CustomWebApplicationFactory.cs
+++ CodeArena.IntegrationTests/Infrastructure/Factories/CustomWebApplicationFactory.cs
@@ -10,23 +10,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace CodeArena.IntegrationTests.Infrastructure.Factories;
 public class CustomWebApplicationFactory<TProgram>
     : WebApplicationFactory<TProgram> where TProgram : class
 {
+    private readonly string _dbName = Guid.NewGuid().ToString();
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
+            var descriptors = services.Where(d =>
+                d.ServiceType == typeof(ApplicationDbContext) ||
+                d.ServiceType == typeof(DbContextOptions) ||
+                d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>))
+                .ToList();
+
+            foreach (var d in descriptors)
+                services.Remove(d);
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(_dbName);
+            });
+
             var descriptor = services.SingleOrDefault(
-                d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
-
-            if (descriptor is not null)
-                services.Remove(descriptor);
-
-            descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(IAntiforgery));
 
             if (descriptor is not null)
@@ -35,10 +45,7 @@ public class CustomWebApplicationFactory<TProgram>
             }
             services.AddSingleton<IAntiforgery, MockAntiforgery>();
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseInMemoryDatabase(Guid.NewGuid().ToString());
-            });
+            
 
             services.AddAntiforgery(options =>
             {
@@ -52,12 +59,8 @@ public class CustomWebApplicationFactory<TProgram>
                 options.DefaultChallengeScheme = TestAuthHandler.SchemeName;
             })
             .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
-                TestAuthHandler.SchemeName, options => { }); 
-
-                var sp = services.BuildServiceProvider();
-                using var scope = sp.CreateScope();
-                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                db.Database.EnsureCreated();
+                TestAuthHandler.SchemeName, options => { });
         });
     }
+    
 }
