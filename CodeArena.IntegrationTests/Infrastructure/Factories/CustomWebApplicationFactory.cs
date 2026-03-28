93271ffcf0679e2cda900a765141e7a545d90416
Martin Terziev <m.terziev08@abv.bg>
2026-03-28 17:16:15 +0200

    Implemented mocks for auth and antiforgery so that post requests are testable with proper assertions.

--- CodeArena.IntegrationTests/Infrastructure/Factories/CustomWebApplicationFactory.cs
+++ CodeArena.IntegrationTests/Infrastructure/Factories/CustomWebApplicationFactory.cs
@@ -1,8 +1,12 @@
 ﻿using CodeArena.Data;
 using CodeArena.Data.Common.Enums;
 using CodeArena.Data.Models;
+using CodeArena.IntegrationTests.Infrastructure.Antiforgery;
+using CodeArena.IntegrationTests.Infrastructure.Auth;
 using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -22,24 +26,38 @@ public class CustomWebApplicationFactory<TProgram>
             if (descriptor is not null)
                 services.Remove(descriptor);
 
-            //descriptor = services.SingleOrDefault(
-            //    d => d.ServiceType == typeof(IAntiforgery));
+            descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(IAntiforgery));
 
-            //if (descriptor is not null)
-            //{
-            //    services.Remove(descriptor);
-            //}
-            //services.AddSingleton<IAntiforgery, MockAntiforgery>();
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+            services.AddSingleton<IAntiforgery, MockAntiforgery>();
+
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseInMemoryDatabase(Guid.NewGuid().ToString());
             });
 
-            var sp = services.BuildServiceProvider();
+            services.AddAntiforgery(options =>
+            {
+                options.Cookie.Name = "__af";
+                options.HeaderName = "X-XSRF-TOKEN";
+            });
 
-            using var scope = sp.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            db.Database.EnsureCreated();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = TestAuthHandler.SchemeName;
+                options.DefaultChallengeScheme = TestAuthHandler.SchemeName;
+            })
+            .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                TestAuthHandler.SchemeName, options => { }); 
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.EnsureCreated();
         });
     }
 }
