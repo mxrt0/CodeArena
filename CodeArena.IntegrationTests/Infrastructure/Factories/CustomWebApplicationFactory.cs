a40cb073611dd435323e36303b44f64440d4060a
Martin Terziev <m.terziev08@abv.bg>
2026-03-28 13:08:35 +0200

    Implemented custom web app factory with configured services.

---
+++ CodeArena.IntegrationTests/Infrastructure/Factories/CustomWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using CodeArena.Data;
+using CodeArena.Data.Common.Enums;
+using CodeArena.Data.Models;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeArena.IntegrationTests.Infrastructure.Factories;
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+            if (descriptor is not null)
+                services.Remove(descriptor);
+
+            //descriptor = services.SingleOrDefault(
+            //    d => d.ServiceType == typeof(IAntiforgery));
+
+            //if (descriptor is not null)
+            //{
+            //    services.Remove(descriptor);
+            //}
+            //services.AddSingleton<IAntiforgery, MockAntiforgery>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            });
+
+            var sp = services.BuildServiceProvider();
+
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+}
