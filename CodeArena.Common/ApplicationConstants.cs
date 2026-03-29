c0344eb6c5bb91aaf3f9096ec911412d9cb2e0e0
Martin Terziev <m.terziev08@abv.bg>
2026-03-29 12:35:00 +0300

    Added cache key constants.

--- CodeArena.Common/ApplicationConstants.cs
+++ CodeArena.Common/ApplicationConstants.cs
@@ -8,4 +8,12 @@ public static class ApplicationConstants
     public const string WarningTempDataKey = "WarningMessage";
     public const string InfoTempDataKey = "InfoMessage";
     public const string SuccessTempDataKey = "SuccessMessage";
+
+    public const string CacheKey_ChallengesAll = "Cache_Challenges_All";
+    public const string CacheKey_ChallengeBySlug = "Cache_Challenge_{0}";
+
+    public const string CacheKey_SubmissionsAll = "Cache_Submissions_All";
+    public const string CacheKey_SubmissionsByUserId = "Cache_Submissions_User_{0}";
+    public const string CacheKey_PendingSubmissions = "Cache_Submissions_Pending";
+    public const string CacheKey_SubmissionById = "Cache_Submission_{0}";
 }
