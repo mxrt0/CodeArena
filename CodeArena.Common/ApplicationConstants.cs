4b32f78690893d60f9820b955493192a6e683211
Martin Terziev <m.terziev08@abv.bg>
2026-03-29 14:00:03 +0300

    Removed unused 'submissions by user id' cache key.

--- CodeArena.Common/ApplicationConstants.cs
+++ CodeArena.Common/ApplicationConstants.cs
@@ -13,7 +13,7 @@ public static class ApplicationConstants
     public const string CacheKey_ChallengeBySlug = "Cache_Challenge_{0}";
 
     public const string CacheKey_SubmissionsAll = "Cache_Submissions_All";
-    public const string CacheKey_SubmissionsByUserId = "Cache_Submissions_User_{0}";
+
     public const string CacheKey_PendingSubmissions = "Cache_Submissions_Pending";
     public const string CacheKey_User_SubmissionById = "Cache_User_Submission_{0}";
     public const string CacheKey_Admin_SubmissionById = "Cache_Admin_Submission_{0}";
