86c9dfb50b7f3204f6dae1670180a60d74448de8
Martin Terziev <m.terziev08@abv.bg>
2026-03-29 18:22:22 +0300

    Ran migration for newly added features.

---
+++ CodeArena.Data/Migrations/20260329144901_ApplicationUser_MakeDisplayNameUnique.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeArena.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationUser_MakeDisplayNameUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedDisplayName",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.Sql(@"
+                UPDATE AspNetUsers
+                SET NormalizedDisplayName = UPPER(DisplayName)
+                WHERE NormalizedDisplayName = '';
+            ");
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 29, 14, 48, 59, 485, DateTimeKind.Utc).AddTicks(4199));
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 29, 14, 48, 59, 485, DateTimeKind.Utc).AddTicks(4205));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_NormalizedDisplayName",
+                table: "AspNetUsers",
+                column: "NormalizedDisplayName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_NormalizedDisplayName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedDisplayName",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 23, 17, 29, 43, 604, DateTimeKind.Utc).AddTicks(4380));
+
+            migrationBuilder.UpdateData(
+                table: "Challenges",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 23, 17, 29, 43, 604, DateTimeKind.Utc).AddTicks(4388));
+        }
+    }
+}
