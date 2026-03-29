1a974fbcda9c78b7aff9bee8a55677da9b381511
Martin Terziev <m.terziev08@abv.bg>
2026-03-29 21:42:15 +0300

    Implemented challenge query wrapper class.

---
+++ CodeArena.Services/QueryModels/ChallengeQuery.cs
@@ -0,0 +1,21 @@
+﻿using CodeArena.Common.Enums;
+using CodeArena.Data.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.QueryModels;
+
+public class ChallengeQuery
+{
+    public int Page { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+
+    public ChallengeStatus Status { get; set; } = ChallengeStatus.All;
+    public Difficulty? Difficulty { get; set; }
+
+    public List<string>? Tags { get; set; }
+    public string? Search { get; set; }
+}
