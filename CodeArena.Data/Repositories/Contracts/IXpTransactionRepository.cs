8268f10a266710b4d18e2bbb0b72a810cbe0c54a
Martin Terziev <m.terziev08@abv.bg>
2026-03-31 19:27:01 +0300

    Created xp transaction repository and implemented essential methods.

---
+++ CodeArena.Data/Repositories/Contracts/IXpTransactionRepository.cs
@@ -0,0 +1,20 @@
+﻿using CodeArena.Data.Common.Enums;
+using CodeArena.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories.Contracts;
+
+public interface IXpTransactionRepository
+{
+    IQueryable<XpTransaction> GetAll();
+    Task AddAsync(XpTransaction transaction);
+
+    Task<bool> AnyAsync(Expression<Func<XpTransaction, bool>> predicate);
+
+    Task SaveChangesAsync();
+}
---
+++ CodeArena.Data/Repositories/XpTransactionRepository.cs
@@ -0,0 +1,40 @@
+﻿using CodeArena.Data.Common.Enums;
+using CodeArena.Data.Models;
+using CodeArena.Data.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Data.Repositories;
+
+public class XpTransactionRepository : IXpTransactionRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public XpTransactionRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(XpTransaction transaction)
+    {
+        await _context.XpTransactions.AddAsync(transaction);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<XpTransaction, bool>> predicate)
+    {
+        return await _context.XpTransactions.AnyAsync(predicate);
+    }
+
+    public IQueryable<XpTransaction> GetAll() => _context.XpTransactions.AsNoTracking();
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
