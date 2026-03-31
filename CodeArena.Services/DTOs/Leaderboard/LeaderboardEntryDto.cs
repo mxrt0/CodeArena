67df4bcc7aaadaa56f25f3c7093a8e47eccc6069
Martin Terziev <m.terziev08@abv.bg>
2026-03-31 20:50:55 +0300

    Added leaderboard entry DTO.

---
+++ CodeArena.Services/DTOs/Leaderboard/LeaderboardEntryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeArena.Services.DTOs.Leaderboard;
+
+public class LeaderboardEntryDto
+{
+    public string UserId { get; set; } = null!;
+    public string DisplayName { get; set; } = null!;
+    public int TotalXp { get; set; }
+    public int ChallengesSolved { get; set; }
+    public int Rank { get; set; }
+}
