c23bf297cfca109f0494cba670933e64610f3736
Martin Terziev <m.terziev08@abv.bg>
2026-03-31 17:20:21 +0300

    Removed unused sortBy property in challenges VM (user)

--- CodeArena.Web/Models/Challenge/ChallengeIndexViewModel.cs
+++ CodeArena.Web/Models/Challenge/ChallengeIndexViewModel.cs
@@ -10,7 +10,6 @@ public class ChallengeIndexViewModel
     public IEnumerable<ChallengeDisplayDto> Challenges { get; set; } = new List<ChallengeDisplayDto>();
     public Difficulty? SelectedDifficulty { get; set; } = null;
     public string? Search { get; set; }
-    public string? SortBy { get; set; }
     public IEnumerable<string> Tags { get; set; } = new List<string>();
     public IEnumerable<string> AvailableTags { get; set; } = new List<string>();
     public ChallengeStatus StatusFilter { get; set; }
