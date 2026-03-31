fda9d7436d00338bb1305b4aea6a7d359ab3994b
Martin Terziev <m.terziev08@abv.bg>
2026-03-31 22:00:31 +0300

    Implement leaderboard MVC logic (controller action, VM and display in view).

---
+++ CodeArena.Web/Controllers/LeaderboardController.cs
@@ -0,0 +1,32 @@
+﻿using CodeArena.Services.Core.Contracts;
+using CodeArena.Web.Models.Leaderboard;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CodeArena.Web.Controllers;
+
+public class LeaderboardController : BaseController
+{
+    const int LeaderboardSize = 10;
+    private readonly ILeaderboardService _service;
+
+    public LeaderboardController(ILeaderboardService service)
+    {
+        _service = service;
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Index()
+    {
+        var leaderboard = await _service.GetLeaderboardAsync(LeaderboardSize);
+
+        var vm = new LeaderboardIndexViewModel
+        {
+            Leaderboard = leaderboard.Data!,
+            CurrentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty
+        };
+
+        return View(vm);
+    }
+}
