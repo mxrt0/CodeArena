181a13966bbf32cecfaefb6be2dc4406913e2c61
Martin Terziev <m.terziev08@abv.bg>
2026-04-06 12:45:07 +0300

    Minor integration test fixes to ensure every test passes.

--- CodeArena.IntegrationTests/Admin/AdminChallengesIntegrationTests.cs
+++ CodeArena.IntegrationTests/Admin/AdminChallengesIntegrationTests.cs
@@ -29,20 +29,6 @@ public class AdminChallengesIntegrationTests
         {
             AllowAutoRedirect = false
         });
-        using var scope = _factory.Services.CreateScope();
-        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        db.Database.EnsureCreated();
-
-        db.Challenges.Add(new Challenge
-        {
-            Id = 1,
-            Title = "Testdfgd",
-            Difficulty = Difficulty.Easy,
-            Description = "Desc12324",
-            Slug = "testdfgd",
-            Tags = "tag123"
-        });
-        db.SaveChanges();
     }
 
     [TearDown]
